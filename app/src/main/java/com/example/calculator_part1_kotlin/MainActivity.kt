db53e4314d2b7500b0048b67618eafb83ec64a79
Koblan <koba.kobe.00@gmail.com>
2020-04-29 15:20:17 +0600

    Init commit.

--- app/src/main/java/com/example/calculator_part1_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculator_part1_kotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculator_part1_kotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.sqrt
@@ -194,8 +195,115 @@ class MainActivity : AppCompatActivity() {
             isOpClicked = false
             screen2.text = ""
         }
+        if (savedInstanceState != null) {
+            screen.setText(savedInstanceState.getString("first_val"))
+            screen2.text = savedInstanceState.getString("operScreen")
+            Operation = savedInstanceState.getChar("operation")
+            isNumclicked = savedInstanceState.getBoolean("isNumClicked")
+            isOpClicked = savedInstanceState.getBoolean("isOpClicked")
+            firstNumber = savedInstanceState.getDouble("firstnumber")
+        }
     }
     fun setTextFields(str: String){
         screen.append(str)
+    }
+
+    fun log10(view: View) {
+        if (isNumclicked && !screen.getText().toString().isEmpty()) {
+            firstNumber = screen.getText().toString().toDouble()
+            if (firstNumber > 0) {
+                res = Math.log10(firstNumber)
+                outp = res.toString()
+                screen.setText(outp)
+            } else {
+                screen2.setText("Error")
+            }
+        }
+    }
+    fun sin(view: View) {
+        if (isNumclicked) {
+            firstNumber = screen.getText().toString().toDouble()
+            res = Math.sin(Math.toRadians(firstNumber))
+            outp = res.toString()
+            screen.setText(outp)
+        }
+    }
+    fun tan(view: View) {
+        if (isNumclicked) {
+            firstNumber = screen.getText().toString().toDouble()
+            if (firstNumber % 90 == 0.0) {
+                screen2.setText("Infinity")
+            } else {
+                res = Math.tan(Math.toRadians(firstNumber))
+                outp = res.toString()
+                screen.setText(outp)
+            }
+        }}
+    fun fact(view: View) {
+        if (isNumclicked) {
+            firstNumber = screen.getText().toString().toDouble()
+            if (firstNumber < 0 || firstNumber > 20) {
+                screen2.setText("Error")
+            } else {
+                var factt: Long = 1
+                var i = 1
+                while (i <= firstNumber) {
+                    factt = factt * i
+                    i++
+                }
+                res = factt.toDouble()
+                outp = res.toString()
+                screen.setText(outp)
+            }
+        }
+    }
+    fun nthroot(view: View) {
+        Operation = 'n'
+        screen2.text = "NthRootOf"
+        if (isNumclicked) {
+            if (isOpClicked == false) {
+                firstNumber = screen.getText().toString().toDouble()
+                screen.setText("")
+            }
+            isOpClicked = true
+            isNumclicked = false
+        }
+    }
+    fun cos(view: View) {
+        if (isNumclicked) {
+            firstNumber = screen.getText().toString().toDouble()
+            res = Math.cos(Math.toRadians(firstNumber))
+            outp = res.toString()
+            screen.setText(outp)
+        }}
+    fun ln(view: View) {
+        if (isNumclicked) {
+            firstNumber = screen.getText().toString().toDouble()
+            if (firstNumber > 0) {
+                res = Math.log(firstNumber)
+                outp = res.toString()
+                screen.setText(outp)
+            } else {
+                screen2.setText("Error")
+            }
+        }}
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("first_val", screen.getText().toString())
+        outState.putString("operScreen", screen2.text.toString())
+        outState.putBoolean("isOpClicked", isOpClicked)
+        outState.putBoolean("isNumClicked", isNumclicked)
+        outState.putChar("operation", Operation)
+        outState.putDouble("firstnumber", firstNumber)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getString("first_val")
+        savedInstanceState.getString("operScreen")
+        savedInstanceState.getBoolean("isOpClicked")
+        savedInstanceState.getBoolean("isNumClicked")
+        savedInstanceState.getChar("operation")
+        savedInstanceState.getDouble("firstnumber")
     }
 }
